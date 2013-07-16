ee169e1aea4f6b15c364fc06a87fb566dc60ed25
Wazir <Wazir@Wazirs-MacBook-Pro.local>
2013-07-16 16:52:09 +0800

    Initial Commit

---
+++ ParseSearchNoPagination/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ParseSearchNoPagination
+//
+//  Created by Wazir on 7/16/13.
+//  Copyright (c) 2013 Wazir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ParseSearchNoPagination/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ParseSearchNoPagination
+//
+//  Created by Wazir on 7/16/13.
+//  Copyright (c) 2013 Wazir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
